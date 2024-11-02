a28dad5bfa949cd786d9a4100b96f28ee73a7b3c
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-11-03 01:20:28 +0530

    commit

---
+++ 01_Basic/03_conversion_Operation.js
@@ -0,0 +1,72 @@
+let score="Lakshman"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let value= Number(score);
+console.log(typeof value);
+console.log(value);
+
+//"33" =>33
+//"33abc"=> NaN
+//true => 1; false=> 0
+
+// let isLogged=1
+
+// let booleanIsLogged= Boolean (isLogged)
+// console.log(typeof booleanIsLogged);
+// console.log(booleanIsLogged)
+
+// 0 => false; 1 => true
+// " " false
+// "lakshman"=> true
+
+// let someNumber=33
+
+// let stringNumber= String(someNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+
+// ****************Operation*****************************************************
+
+// let values=3;
+// let negValue=-values;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2%3);
+
+
+let str1= "Lakshman"
+let str2=" Ram"
+let  str3= str1+str2
+console.log(str3);
+
+// console.log("1"+2);
+// console.log(1 + "2");
+// console.log("1" + 2 +2);
+// console.log(1 +2 +"2");
+
+// console.log((3+4)*5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
