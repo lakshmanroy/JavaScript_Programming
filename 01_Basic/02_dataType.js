404167936bd23177676295295949e631bb5f74b2
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-11-03 01:19:51 +0530

    commit

---
+++ 01_Basic/02_dataType.js
@@ -0,0 +1,24 @@
+"use strick";  //theat all js code as newer version
+
+//alert(3+3) //we are using node js,not brower
+
+console.log(3+3);
+               //code redability should be high
+console.log("Lakshman");
+
+let age=18;
+let isLogged=false;
+let state;  //undefind 
+
+//number => 2 to power 53
+//BigInt
+//String ""
+//boolean => true/false
+//null => standalone value
+//undefind
+//symbol => unique
+//object
+
+
+console.log(typeof undefined);
+console.log(typeof "Lakshman");
