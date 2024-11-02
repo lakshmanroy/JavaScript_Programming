9a29a6d5274d19fc9a9836ac09785f10d2706024
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-11-03 01:19:11 +0530

    commit

---
+++ 01_Basic/01_Variable.js
@@ -0,0 +1,15 @@
+const accountId=12345;
+let accountEmail= "lakshman@google.com";
+var accountPassword="54332";
+accountCity="sheohar";
+let accountState;
+
+
+accountEmail="raju@gmail.com"
+accountPassword="77777";
+accountCity="chennai"
+
+//prefer not use var
+ //because of issue in block scop and functionals scop
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
