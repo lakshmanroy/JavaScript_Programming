5b7daebb2842a2e0a053c0d87e21dfe5d69fcd00
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-11-03 01:21:33 +0530

    commit

---
+++ 01_Basic/05_String.js
@@ -0,0 +1,45 @@
+const name= "Lakshman";
+const repoCount= 50;
+
+// console.log(name + repoCount + "value");
+
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName= new String('hitesh-hc-pc')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+// const newString= gameName.substring(0,4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8,4)
+// console.log(anotherString);
+
+
+
+
+
+const s= "      raj    "
+console.log(s);
+console.log(s.trim());
+
+const url= "https://google.com"
+console.log(url.replace('20%','-'));
+console.log(url.includes('raj'));
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
+
+
