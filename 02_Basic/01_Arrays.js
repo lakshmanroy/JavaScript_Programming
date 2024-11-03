90818709dd6dba3c5dff8d81d8cbd41e4ff51933
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-11-03 15:09:27 +0530

    commit

---
+++ 02_Basic/01_Arrays.js
@@ -0,0 +1,42 @@
+const myArr= [1,2,3,4];
+const name=["Lakshman","Ravi","saktiman"]
+
+// console.log(myArr[0]);
+const myArr2=new Array(1,2,3,4)
+// console.log(myArr2[1]);
+
+//Arrays methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+// console.log(myArr);
+
+// myArr.unshift(9)
+// myArr.shift()
+// console.log(myArr);
+
+const newArr=myArr.join()
+// console.log(myArr);
+// console.log(newArr);
+
+
+//slice,slice
+
+console.log("A",myArr);
+const myn1=myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B",myArr);
+
+const myn2=myArr.splice(1,3)
+console.log("c",myArr);
+
+console.log(myArr2);
+
+
+
+
+
+
+
