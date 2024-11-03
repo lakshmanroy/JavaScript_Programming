194c38c2b8108a8f5f5695e2e753cea1a3bf13e2
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-11-03 16:23:00 +0530

    commit

---
+++ 02_Basic/02_Arrays.js
@@ -0,0 +1,28 @@
+const name= ["Lakshman","Riya","Ravi","Mahesh"]
+const hero= ["superMan","flash","batman"]
+
+name.push(hero)
+// console.log(name);
+
+// const allNew= [...name,...hero]
+// console.log(allNew);
+
+// const another_Arrays= [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const real_another_Arrays=another_Arrays.flat(Infinity)
+// console.log(real_another_Arrays);
+
+// console.log(Array.isArray("Lakshman"));
+// console.log(Array.from("Lakshman"));
+// console.log(Array.from({name:"Lakshman"}));
+
+let score=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score,score2,score3));
+
+
+
+
+
+
