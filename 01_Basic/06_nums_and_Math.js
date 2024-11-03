a7421a3dbb425119d56f989010e51de2e1451418
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-11-03 11:29:41 +0530

    commit

---
+++ 01_Basic/06_nums_and_Math.js
@@ -0,0 +1,46 @@
+const score=400
+// console.log(score);
+
+const balance= new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const a= 123.897;
+// console.log(a.toPrecision(3));
+
+const hundred=10000
+// console.log(hundred.toLocaleString('en-IN'));
+
+//*********************Math************************** */
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.2));
+// console.log(Math.max(3,4,5,1,9,4));
+// console.log(Math.min(4,6,8,3,1,7));
+// console.log(Math.floor(4.2));
+// console.log(Math.ceil(4.5));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+//import 
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
+
+
+
+
+
