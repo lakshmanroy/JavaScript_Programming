ecf8f29f495c119779213c051ce82353197d0623
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-11-04 18:30:58 +0530

    commit

--- 03_Basic/02_Scopes.js
+++ 03_Basic/02_Scopes.js
@@ -12,4 +12,45 @@ if(true){
 // console.log(b);
 // console.log(c);
 
+//**************************************** */
+
+// function one(){
+//     const username= "Lakshman"
+//     function two(){
+//         const website= "Youtube"
+//         console.log(username);
+//     }
+//     // console.log(website);
+    
+//     two()
+// }
+// one()
+
+//********************************************** */
+
+if(true){
+    const username = "Lakshman"
+    if(username==="Lakshman"){
+        const website= "youtube"
+        console.log(username+website);  
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+//********************************************* */
+
+
+//***********interesting*****************8 */
+
+function addone(nums){
+     return nums+1
+}
+console.log(addone(5));
+
+
+const addTwo= function(nums){
+    return nums+2
+}
+console.log(addTwo(5));
 
