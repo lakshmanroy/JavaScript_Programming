ad4f8337a166b11bb8896dcf3d1e114df72bacd2
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-11-04 17:01:03 +0530

    commit

---
+++ 03_Basic/01_Functions.js
@@ -0,0 +1,22 @@
+function Hello(){
+    console.log("H");
+    console.log("e");
+    console.log("l");
+    console.log("l");
+    console.log("o");
+    console.log("Hii");
+    
+}
+// Hello()
+
+function addTwoNumber(number1,number2){
+    // console.log(number1+number2);
+    // let result= number1+number2
+    // return result;
+
+    return number1+number2
+    
+}
+ const result = addTwoNumber(4,3)
+ console.log("result:",result);
+ 
