3b5cf95df6596c560cd494f58480851c44a8c39d
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-11-05 13:38:12 +0530

    commit

---
+++ 03_Basic/04_life.js
@@ -0,0 +1,11 @@
+//immediately invokes funciton express (LIFE)
+
+(function chai(){
+    console.log(`DB CONNECTED`);
+    
+}
+ ());
+
+( (name)=>{
+    console.log(`DB CONNECTED TWO ${name}`); 
+ })('Lakshman')
