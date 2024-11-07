7e9f5db62264adb6d3840176e7cc5bb33eabeaa6
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-11-07 12:27:21 +0530

    commit

---
+++ Control_Flow/one.js
@@ -0,0 +1,20 @@
+//if
+
+const result= true;
+const age=18;
+// if(age===50){
+//     console.log("less than 50");
+// }
+// else{
+//     console.log("greater than 50");  
+// }
+// console.log("excute");
+
+//************************* */
+
+const score=200;
+if(score>100){
+    var power ="fly";
+    console.log(`user score: ${power}`); 
+}
+console.log(`user power ${power}`);
