fa1618baa1926706ee47028f181dcbb3dde532d3
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-11-11 22:13:59 +0530

    commit

--- Control_Flow/one.js
+++ Control_Flow/one.js
@@ -20,17 +20,32 @@ const age=18;
 // // console.log(`user power ${power}`);
 
 //*************************** */
-const balance=1000;
+// const balance=1000;
 
-if(balance<500){
-    console.log("less than 500");  
+// if(balance<500){
+//     console.log("less than 500");  
+// }
+// else if(balance<750){
+//     console.log("less than 750");   
+// }
+// else if(balance<900){
+//     console.log("less than 900"); 
+// }
+// else{
+//     console.log("less than 1200");
+// }
+
+//****************************** */
+
+const userLoggedIn=true
+const debitcard=true
+const LoggedFromGoogle=false;
+const loggedFromEmail=true;
+
+if(userLoggedIn && debitcard){
+    console.log("Allow to buy courses");  
 }
-else if(balance<750){
-    console.log("less than 750");   
-}
-else if(balance<900){
-    console.log("less than 900"); 
-}
-else{
-    console.log("less than 1200");
+if(LoggedFromGoogle || loggedFromEmail){
+    console.log("user logged in");
+    
 }
