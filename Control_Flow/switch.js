a04255551efec4216a22565497dbabd8f76da03e
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-11-11 22:25:36 +0530

    commit

---
+++ Control_Flow/switch.js
@@ -0,0 +1,28 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+const month="April"
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "April":
+        console.log("april");
+        break;
+
+    default:
+        console.log("invalid");
+        
+        break;
+}
