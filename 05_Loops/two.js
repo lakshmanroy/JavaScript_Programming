b2bae7d2a12bd5dda1483dc75479df424a76253c
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-11-11 23:57:57 +0530

    commit

---
+++ 05_Loops/two.js
@@ -0,0 +1,22 @@
+// let i=0;
+// while(i<=20){
+//     console.log(`value of the ${i}`);
+//     i=i+2; 
+// }
+
+// let myArray=  ["Lakshman","pawan","kajal"];
+
+// let arr=0;
+// while(arr<myArray.length){
+//     console.log(`value is ${myArray[arr]}`);
+//     // arr++;
+//     arr=arr+1;
+    
+// }
+
+let score=11;
+do{
+    console.log(`score is ${score}`);
+    score++;
+}
+while(score<=10);
