e71e7a249c32985770d2138ec63171b1a10c3041
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-11-11 23:00:55 +0530

    commit

---
+++ Control_Flow/truthy.js
@@ -0,0 +1,43 @@
+const userEmail="Lakshman@mail.com"
+if(userEmail){
+    console.log("userEmail");
+}
+else{
+    console.log("dont have user email");
+}
+
+//fulsy values
+
+
+//false,0,-0,BigInt on,"",null,undefined,NaN
+
+//truthy values
+//"0",'false',[],{},functions
+
+// const emptyObj={};
+// if(Object.keys(emptyObj).length===0){
+//   console.log("Object.is emptyObj");
+// }
+
+//Nullish coalecript Operator (??):null undefined
+
+let val1;
+// val1= 5??10
+// val1= null??10
+// val1=undefined??10
+
+console.log(val1);
+
+//********************** */
+
+//ternary operator
+
+//condition ? true:false
+
+const icecream=100
+icecream<=80?console.log("less than 80"):console.log("more than 80");
+
+
+
+
+
