8666a92b71db36d444e482a735bfcfd2f6acaf64
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-11-12 21:24:34 +0530

    commit

---
+++ 05_Loops/five.js
@@ -0,0 +1,32 @@
+const coding= ["js","rubby","java","python","javaScript"];
+
+// coding.forEach(function(val) {
+//     console.log(val);
+    
+// })
+
+
+//call back method
+coding.forEach((item)=> {
+    // console.log(item);  
+})
+
+const myCoding= [
+    {
+         name: "Rohit",
+         fileName: "full stack developer"
+    },
+    {
+         name: "Deepak",
+         fileName: "web developer"
+    },
+    {
+         name: "sumit",
+         fileName: "mern stack developer"
+    },
+]
+
+myCoding.forEach( (item)=>{
+    console.log(item.name);
+    
+})
