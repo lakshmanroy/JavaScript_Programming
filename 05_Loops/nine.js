82f36693858f9ff67b0190a240203ddf81c049f6
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-11-13 23:57:19 +0530

    commit

--- 05_Loops/nine.js
+++ 05_Loops/nine.js
@@ -1,10 +1,27 @@
 const myNum= [1,2,3]
 
 const myTotal=myNum.reduce(function (key,value){
-    console.log(`key: ${key} and value :${myNum}`);
+    // console.log(`key: ${key} and value :${myNum}`);
     
     return key+value
 },0)
 
-console.log(myTotal);
+// console.log(myTotal);
+
+const shoppingCart= [
+    {
+        itemName: "js course",
+        prices: 2999
+    },
+    {
+        itemName: "py course",
+        prices: 999
+    },
+    {
+        itemName: "java course",
+        prices: 299
+    },
+]
+const pricesToPay= shoppingCart.reduce((acc,item) => acc+item.prices,0)
+console.log(pricesToPay);
 
